d2f203a5f82aff0eb84fcd9d2e776998ad2a9b3b
Guardezi <jefersonguardezi@gmail.com>
2014-11-11 13:56:02 -0200

    corrigido o problema de quebra de linha

--- programas/comparacao/teste.py
+++ programas/comparacao/teste.py
@@ -1,19 +1,17 @@
 #!/bin/python
 from os import popen
 
-saida = open("run.sh",'w')
-
 det = ["HARRIS","MSER"]
 desc = ["SIFT"]
 
 for a in det:
 	for d in desc:
-		teste = open("../testes/testes.txt")
+		teste = open("testes.txt")
 		for t in teste:
 			if len(t) > 1:
-				base = open("../testes/base.txt")
+				base = open("base.txt")
 				for b in base:
 					if len(b) > 1:
-						txt = str("./comparacao/compara {} {} {} {} >> resultado.txt".format(t,b,a,d))
+						txt = str("./comparacao/compara {0} {1} {2} {3} >> resultado.txt".format(t,b,a,d))
 						print txt
 						popen(txt)
