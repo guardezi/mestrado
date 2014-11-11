9723a14443a8cb4e582d6c9e84eae50c49080ad5
Guardezi <jefersonguardezi@gmail.com>
2014-11-11 13:08:26 -0200

    Ajuste teste

--- programas/ajustaTeste.py
+++ programas/ajustaTeste.py
@@ -1,6 +1,6 @@
 #!/bin/python
 
-arquivoSaida = open("saida.txt",'w')
+saida = open("run.sh",'w')
 
 det = ["HARRIS","MSER"]
 desc = ["SIFT"]
@@ -13,7 +13,8 @@ for a in det:
 				base = open("../testes/base.txt")
 				for b in base:
 					if len(b) > 1:
-						print "./comparacao/compara "+t+" "+b+" "+a+" "+d
+						print "./comparacao/compara {} {} {} {}".format(t,b,a,d)
+						saida.write("./comparacao/compara {} {} {} {}\n".format(t,b,a,d))
 
 
 
