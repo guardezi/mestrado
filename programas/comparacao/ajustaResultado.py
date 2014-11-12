555a4d00b16b9cb50d12939fd51c34125f3a81c6
Guardezi <jefersonguardezi@gmail.com>
2014-11-12 01:58:36 -0200

    Ajusta resultados precisa adaptação para outros algorimto funciona para HARRIS E MSER

--- programas/comparacao/ajustaResultado.py
+++ programas/comparacao/ajustaResultado.py
@@ -1,26 +1,89 @@
 #!/bin/python
 
-arquivo = open("resultados.txt")
-arquivoSaida = open("saida.txt",'w')
+def ordena( arquivo ):
+	dic = []
+	arquivo = arquivo.split("\n")
+	for linha in arquivo:
+		separado = linha.split("\t")
+		separado[2] = int(separado[2])
+		separado[1] = separado[1].split("-")[0]
+		dic.append(separado)
 
-for arq in arquivo:
-	#print arq
-	ponto = arq.split(".")
-	#	print ponto	
-	teste=arq.split("\t")
-	print teste
-	if len(teste) > 1:
-		detector  = teste[2]
-		descritor = teste[3]
-		pontoA 	  = teste[4]
-		pontoB 	  = teste[5]
-		match  	  = teste[6] 
+	dic.sort(key=lambda x: x[2])
+	# print dic[len(dic)-1]
+	# print dic[len(dic)-2]
+	# for d in dic:
+	# 	print d
+	return dic	
 
+if __name__ == '__main__':
+	arquivo = open("resultado.txt")
+	arquivoSaida = open("resultadoAjustado.txt",'w')
 
-	if len(ponto) > 1:
-		final = ponto[0].split("/")
-		recorte = arq.split("/")
-		arquivoSaida.write(final[-1] + ".png " + recorte[-1])
+	harris = []
+	mser =[]
 
-arquivoSaida.close()
+	ultimo="vazio"
+	cont=0
+	txt=""
+	print txt
+
+	for arq in arquivo:
+		ponto = arq.split(".")
+		teste=arq.split("\t")
+		if len(teste) > 1:
+			imgA      = teste[0].split("/")
+			imgA	  = imgA[len(imgA)-1]
+			imgA      = imgA.split(".")[0]
+			
+			imgB      = teste[1].split("/")
+			imgB	  = imgB[len(imgB)-1]
+			imgB      = imgB.split(".")[0]      
+			detector  = teste[2]
+			descritor = teste[3]
+			pontoA 	  = teste[4]
+			pontoB 	  = teste[5]
+			match  	  = teste[6] 
+			
+			if imgA != ultimo:
+				# print cont
+				if cont != 0:
+					if detector == "HARRIS":
+						harris.append(ordena(txt))
+					if detector == "MSER":
+						mser.append(ordena(txt))
+					
+				ultimo = imgA
+				cont +=1
+				txt=imgA+"\t"+imgB+"\t"+match
+			else:
+				txt = txt + "\n" + imgA+"\t"+imgB+"\t"+match
+	c=int(0)
+	e=int(0)
+	for i in xrange(0,len(harris)):
+		a = harris[i][len(harris[i])-1]
+		if a[0].split("-")[0] == a[1]:
+			# print a[0].split("-")[0]+" "+a[1]+" "+str(a[2])
+			c+=1
+		else :
+			e+=1
+	print "HARRIS\ntotal ",(c+e)
+	print "Corretos {} {}%".format(c,(100*c/(c+e)))
+	print "errados {} {}%".format(e,(100*e/(c+e)))
+
+	c=int(0)
+	e=int(0)
+	for i in xrange(0,len(mser)):
+		a = mser[i][len(mser[i])-1]
+		if a[0].split("-")[0] == a[1]:
+			# print a[0].split("-")[0]+" "+a[1]+" "+str(a[2])
+			c+=1
+		else :
+			e+=1
+	print "\nMSER\ntotal ",(c+e)
+	print "Corretos {} {}%".format(c,(100*c/(c+e)))
+	print "errados {} {}%".format(e,(100*e/(c+e)))
+
+	arquivoSaida.close()
+
 
